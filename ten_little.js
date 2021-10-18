0662213c5ddef291edcb4714d7d9db9d754aadc5
lucaolmrova <81082745+lucaolmrova@users.noreply.github.com>
2021-10-18 20:00:26 +0200

    Update ten_little.js

--- ten_little.js
+++ ten_little.js
@@ -1,14 +1,14 @@
 for (let i = 1; i <= 10; i++){
   if (i % 3 === 0 ){
-    console.log (i + ' little indians, ')
+    console.log (i + ' little indians,\n ')
   } else {
     console.log (i + ' little ')
   }
-} console.log (' indian boys. ')
+} console.log (' indian boys.\n ')
 
 for (let i = 10; i > 0; i--){
   if ((i === 8) || (i === 5 ) || (i === 2)){
-    console.log (i + ' little indians, ')
+    console.log (i + ' little indians,\n ')
   } else
   console.log (i + ' little ')
   }
